dbe17538cbc7f94b73dc6e5793105781b8b686ec
Ran Tao <rantaoca@gmail.com>
2015-05-17 19:02:30 -0700

    Fix some typos

--- Pollr/models/Response.swift
+++ Pollr/models/Response.swift
@@ -7,15 +7,6 @@
 //
 
 import Foundation
-//
-//  Response.swift
-//  Pollr
-//
-//  Created by Ran Tao on 5/15/15.
-//  Copyright (c) 2015 Kabir. All rights reserved.
-//
-
-import Foundation
 
 typealias Time = String
 typealias PropertyList = [NSObject: AnyObject]
--- Pollr/models/User.swift
+++ Pollr/models/User.swift
@@ -7,15 +7,6 @@
 //
 
 import Foundation
-//
-//  poll.swift
-//  Pollr
-//
-//  Created by Ran Tao on 5/15/15.
-//  Copyright (c) 2015 Kabir. All rights reserved.
-//
-
-import Foundation
 
 typealias Time = String
 typealias PropertyList = [NSObject: AnyObject]
