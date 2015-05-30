acfc2204aa8f79fda2cb9bb4a27bce92e94d85f2
Ran Tao <rantaoca@gmail.com>
2015-05-29 20:43:04 -0700

    Add pods objective c bridging header

---
+++ Pollr/Pods-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Pods-Bridging-Header.h
+//  Pollr
+//
+//  Created by Ran Tao on 5/29/15.
+//  Copyright (c) 2015 Kabir. All rights reserved.
+//
+
+#ifndef Pollr_Pods_Bridging_Header_h
+#define Pollr_Pods_Bridging_Header_h
+
+#import <DateTools/NSDate+DateTools.h>
+
+#endif
