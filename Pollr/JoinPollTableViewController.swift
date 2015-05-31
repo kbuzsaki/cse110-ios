96bd7a36480ae621c7eeefa8d146dd8758efce00
Ran Tao <rantaoca@gmail.com>
2015-05-31 10:42:57 -0700

    Added join poll views, doesn't send the poll code to the server yet.

---
+++ Pollr/JoinPollTableViewController.swift
@@ -0,0 +1,126 @@
+//
+//  JoinPollTableViewController.swift
+//  Pollr
+//
+//  Created by Ran Tao on 5/31/15.
+//  Copyright (c) 2015 Kabir. All rights reserved.
+//
+
+import UIKit
+
+class JoinPollTableViewController: UITableViewController {
+
+    @IBOutlet weak var enterCodeField: UITextField!
+    
+    var joinButton = UIBarButtonItem(title: "Join", style: .Plain, target: nil, action: "validateCode")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        enterCodeField.becomeFirstResponder()
+        
+        // Create the upper right corner join button.
+        joinButton.enabled = false
+        self.navigationItem.rightBarButtonItem = joinButton
+        
+        // Enable/disable join button when text is entered.
+        enterCodeField.addTarget(self, action: "enterCodeFieldChanged", forControlEvents: UIControlEvents.EditingChanged)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 1
+    }
+    
+    func validateCode() {
+        let code = enterCodeField.text
+        
+    }
+    
+    func enterCodeFieldChanged() {
+        if enterCodeField.text.isEmpty {
+            if joinButton.enabled {
+                joinButton.enabled = false
+            }
+        } else {
+            if !joinButton.enabled {
+                joinButton.enabled = true
+            }
+        }
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
